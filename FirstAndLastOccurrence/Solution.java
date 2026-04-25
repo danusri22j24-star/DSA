5e5aa72f4e89a95c0d745b399d264ef4142dea45
danuu <codeiodemo@gmail.com>
2026-04-25 22:31:04 +0530

    First and Last Occurrence

---
+++ FirstAndLastOccurrence/Solution.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int[] searchRange(int[] nums, int target) {
+        int start = 0;
+        int end = nums.length - 1;
+        int mid=0;
+
+        while(start <= end){
+            mid = (start + end)  / 2;
+            if(nums[mid] == target) {
+                int left = mid;
+                int right = mid;
+                while(left >= 0 && nums[left] == target){
+                    left--;
+                }
+                while(right < nums.length && nums[right] == target){
+                    right++;
+                }
+                return new int [] {left + 1, right - 1};
+            }
+            if(nums[mid] > target) {
+                end = mid - 1;
+            }
+            if(nums[mid] < target) {
+                start = mid + 1;
+            }
+        }
+        return new int [] {-1 , -1};
+    }
+}
